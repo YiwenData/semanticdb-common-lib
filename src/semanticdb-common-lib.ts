4d815f0677d352df25437ead5ae6a8a722c7eb15
kilik52 <kilik52@hotmail.com>
2021-07-25 17:21:46 +0800

    feat: sort funcion

--- src/semanticdb-common-lib.ts
+++ src/semanticdb-common-lib.ts
@@ -1,20 +1,44 @@
-
 /**
- * 
+ *
  * @param arr Array or item
  * @param func function on each item
  */
 const each = async (arr: any, func: Function) => {
-  if (arr === null || arr === undefined) return null;
+  if (arr === null || arr === undefined) return null
 
   if (Array.isArray(arr)) {
-    const result = await Promise.all(arr.map(i => func(i)));
-    return result;
+    const result = await Promise.all(arr.map(i => func(i)))
+    return result
   }
 
-  return func(arr);
-};
+  return func(arr)
+}
+
+/**
+ *
+ * @param arr Array or item
+ * @param direction number
+ * @param keys multiple sorting keys
+ */
+const sort = (arr: any[], direction: number, keys: string[]) => {
+  return arr.sort((itemA, itemB) => {
+    for (const key of keys) {
+      const a = itemA[key]
+      const b = itemB[key]
+      if (a === b) {
+        continue
+      }
+
+      if (typeof a === 'number') {
+        return direction === 1 ? a - b : b - a
+      }
+
+      return direction === 1 ? a.localeCompare(b) : b.localeCompare(a)
+    }
+  })
+}
 
 export default {
   each,
+  sort
 }
--- test/semanticdb-common-lib.test.ts
+++ test/semanticdb-common-lib.test.ts
@@ -1,35 +1,70 @@
-import _ from "../src/semanticdb-common-lib"
+import _ from '../src/semanticdb-common-lib'
 
 /**
  * test
  */
-describe("test", () => {
-  it("each", async () => {
-    const arr = [1, 2, 3];
-    const item = 1;
+describe('test', () => {
+  it('each', async () => {
+    const arr = [1, 2, 3]
+    const item = 1
 
-    const nullRet = await _.each(null, (item: any) => item + 1);
-    expect(nullRet).toBeNull();
+    const nullRet = await _.each(null, (item: any) => item + 1)
+    expect(nullRet).toBeNull()
 
-    const arrRet = await _.each(arr, (item: any) => item + 1);
-    expect(arrRet).toEqual([2, 3, 4]);
+    const arrRet = await _.each(arr, (item: any) => item + 1)
+    expect(arrRet).toEqual([2, 3, 4])
 
-    const itemRet = await _.each(item, (item: any) => item + 1);
-    expect(itemRet).toEqual(2);
+    const itemRet = await _.each(item, (item: any) => item + 1)
+    expect(itemRet).toEqual(2)
 
     const asyncFunc = (item: number) => {
       const promise = new Promise((resolve, reject) => {
         setTimeout(() => {
-          resolve(item + 1);
-        }, 100);
-      });
-      return promise;
+          resolve(item + 1)
+        }, 100)
+      })
+      return promise
     }
 
-    const arrRetAsync = await _.each(arr, asyncFunc);
-    expect(arrRetAsync).toEqual([2, 3, 4]);
+    const arrRetAsync = await _.each(arr, asyncFunc)
+    expect(arrRetAsync).toEqual([2, 3, 4])
 
-    const itemRetAsync = await _.each(item, asyncFunc);
-    expect(itemRetAsync).toEqual(2);
+    const itemRetAsync = await _.each(item, asyncFunc)
+    expect(itemRetAsync).toEqual(2)
+  })
+
+  it('sort', async () => {
+    const arr = [
+      {
+        name: 'bac',
+        number: 2
+      },
+      {
+        name: 'bac',
+        number: 10
+      },
+      {
+        name: 'abc',
+        number: 2
+      },
+      {
+        name: 'abc',
+        number: 1
+      }
+    ]
+
+    expect(_.sort(arr, 1, ['name', 'number'])).toEqual([
+      { name: 'abc', number: 1 },
+      { name: 'abc', number: 2 },
+      { name: 'bac', number: 2 },
+      { name: 'bac', number: 10 }
+    ])
+
+    expect(_.sort(arr, -1, ['name', 'number'])).toEqual([
+      { name: 'bac', number: 10 },
+      { name: 'bac', number: 2 },
+      { name: 'abc', number: 2 },
+      { name: 'abc', number: 1 }
+    ])
   })
 })
